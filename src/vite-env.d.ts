514cf2b77cbd55f65e17287cd6fe58562e8f2bd5
William Victor <vicken408@gmail.com>
2025-11-30 11:51:22 +0100

    base url

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,5 +1,13 @@
 /// <reference types="vite/client" />
 
+interface ImportMetaEnv {
+  readonly VITE_API_BASE_URL?: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
+
 declare module 'react' {
   export type Dispatch<A> = (value: A) => void;
   export type SetStateAction<S> = S | ((prevState: S) => S);
